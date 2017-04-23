4722cf4e20566d7800bd911ef598ba80c4682c09
colo76 <martinbaez@gmail.com>
2017-04-23 13:21:49 -0300

    Parametros en modulos

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -2,21 +2,29 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 /*
-* Módulo simple que imprime en el log un mensaje
+* Módulo simple que: 
+* Imprime en el log un mensaje
 *
 *
 */
 
+
+/* Indicamos la licencia del módulo */
+MODULE_LICENSE("GPL"); 
+
+/* Función que se invoca cuando se carga el módulo en el kernel */
 static int hello_so_init(void){
    printk(KERN_INFO "Hola SO2017 \n");
+   /* Devolver 0 para indicar una carga correcta del módulo */
    return 0;
 }
 
+/* Función que se invoca cuando se descarga el módulo del kernel */
 static void hello_so_exit(void){
    printk(KERN_INFO "Chau SO2017 \n");
 }
 
 
-//Inicializamos las funciones de de carga y descarga
+//Declaramos las funciones de de carga y descarga
 module_init(hello_so_init);
 module_exit(hello_so_exit);
--- hello_world2/hello2.c
+++ hello_world2/hello2.c
@@ -1,28 +1,41 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+
+#include <linux/moduleparam.h>
+
 /*
 * Módulo simple que: 
 *  Imprime en el log un mensaje
-*  Recibe parámetros
-*  Exporta símbolos: 
+*  Recibe parámetros desde el boot o desde la carga por parte del usuario 
+*  Exporta algunos símbolos
 *
 */
 
 
 /* Indicamos la licencia del módulo */
 MODULE_LICENSE("GPL"); 
+MODULE_AUTHOR("Martín Baez");
+MODULE_DESCRIPTION("Ejemplo - SO2017");
+
+/* Configuramos el parametro que recibe desde la carga del módulo */
+static char *user_name = "";
+module_param(user_name, charp, 0);  
+MODULE_PARM_DESC(user_name, "El nombre del usuario");
 
 /* Función que se invoca cuando se carga el módulo en el kernel */
 static int hello_so_init(void){
-   printk(KERN_INFO "Hola SO2017 \n");
+
+
+    printk(KERN_INFO "Hola %s \n",user_name);
+  
    /* Devolver 0 para indicar una carga correcta del módulo */
    return 0;
 }
 
 /* Función que se invoca cuando se descarga el módulo del kernel */
 static void hello_so_exit(void){
-   printk(KERN_INFO "Chau SO2017 \n");
+   printk(KERN_INFO "Chau %s",user_name);
 }
 
 
